523c75f05953be4f800d99edccf9dc3bf5ae91cf
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-05-12 09:24:10 -0300

    feat: db setup

--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DatabaseModule],
 })
 export class HttpModule {}
