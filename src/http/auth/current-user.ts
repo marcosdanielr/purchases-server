bddec6e077242991bdd2a312569de07a335a8cad
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-05-13 18:11:29 -0300

    feat: current user decorator

---
+++ src/http/auth/current-user.ts
@@ -0,0 +1,15 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+export interface AuthUser {
+  sub: string;
+}
+
+export const CurrentUser = createParamDecorator(
+  (_: unknown, context: ExecutionContext): AuthUser => {
+    const ctx = GqlExecutionContext.create(context);
+    const req = ctx.getContext().req;
+
+    return req.user;
+  },
+);
